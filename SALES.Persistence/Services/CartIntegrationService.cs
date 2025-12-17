143fbbb202cfa1ff35b17a5f5f5621abe1f135b2
EULERLOSVIN <eulerlosvinmartinezhurtado@gmail.com>
2025-12-17 16:43:52 -0500

    Descomentamos 2 servicios para correr pruebas

--- SALES.Persistence/Services/CartIntegrationService.cs
+++ SALES.Persistence/Services/CartIntegrationService.cs
@@ -1,31 +1,31 @@
-﻿//using SALES.Application.Interfaces;
-//using System.Net.Http;
-//using System.Net.Http.Json;
-//using System.Threading.Tasks;
+﻿using SALES.Application.Interfaces;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
 
-//namespace SALES.Persistence.Services
-//{
-//    public class CartIntegrationService : ICartIntegrationService
-//    {
-//        private readonly HttpClient _httpClient;
+namespace SALES.Persistence.Services
+{
+    public class CartIntegrationService : ICartIntegrationService
+    {
+        private readonly HttpClient _httpClient;
 
-//        public CartIntegrationService(HttpClient httpClient)
-//        {
-//            _httpClient = httpClient;
-//        }
+        public CartIntegrationService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
 
-//        public async Task<bool> CloseCartAsync(int idCart)
-//        {
-//            try
-//            {
-//                var body = new { IdCart = idCart, IdState = 2 };
-//                var response = await _httpClient.PutAsJsonAsync("/Cart/update-state", body);
-//                return response.IsSuccessStatusCode;
-//            }
-//            catch
-//            {
-//                return false;
-//            }
-//        }
-//    }
-//}
+        public async Task<bool> CloseCartAsync(int idCart)
+        {
+            try
+            {
+                var body = new { IdCart = idCart, IdState = 2 };
+                var response = await _httpClient.PutAsJsonAsync("/Cart/update-state", body);
+                return response.IsSuccessStatusCode;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
